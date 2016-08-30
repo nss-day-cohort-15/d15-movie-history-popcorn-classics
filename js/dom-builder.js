dded51408548a44c7a131e29af349a2c3302134b
Jacob Keen <jaclkeen@gmail.com>
2016-08-30 09:01:17 -0500

    trying to make things work

--- js/dom-builder.js
+++ js/dom-builder.js
@@ -1,10 +1,7 @@
 "use strict";
 var $ = require('../bower_components/jquery/dist/jquery.min.js')
-    // Handlebars = require('/hbsfy/runtime'),
-    // entryTemplate = require('../templates/article.hbs')
-    // entryData = require('../templates/data.js')
 
-function addToDom(data){
+function addSearchToDom(data){
   //USED TO PREVENT APP BREAK WHEN RESULTS RETURNED IS
   // LESS THAN 3
   var times
@@ -23,8 +20,36 @@ function addToDom(data){
 
     var movie = `<div class='movie'>${title}${poster}${year}${add}</div>`
     $('#homemovies').append(movie)
-  // $('#homemovies').append(entryTemplate(entryData))
   }
 }
 
-module.exports = {addToDom}
+function addYoursToDom(data){
+ for(var key in data){
+    var destroy = `<span class='delete glyphicon glyphicon-remove'></span>`
+    var title = `<h5 class='movie_title'>${data[key].title}</h2>`
+    var poster = `<img src='${data[key].poster}' class='poster'>`
+    var year = `<p class='year'>${data[key].year}</p>`
+    var rate = `<div class='ratings' id='ratings'><input class='rating' id='rating'
+    type='range' step='.5' value='0' min='0' max='10'><span class='r_value'>0</span></div>`
+    var add = `<p><a href='#' class='save' id='save'>Save Rating</a></p>`
+
+
+    var movie = `<div class='movie'>${destroy}${title}${poster}${year}${rate}${add}</div>`
+    // $('#homemovies').append(movie)
+
+  if(data[key].watched === false && data[key].rating < 1){
+    $('#unwatchedmovies').append(movie)
+  }else if(data[key].watched === true || data[key].rating > 0){
+    $('#watchedmovies').append(movie)
+  }
+ }
+  $('.delete').on('click', function(){
+    $(this).parent().remove()
+  })
+
+  $('.ratings').on('input', function(){
+    $(this).children('.r_value').html($('#rating', this).val())
+  })
+}
+
+module.exports = {addSearchToDom, addYoursToDom}
--- js/main.js
+++ js/main.js
@@ -8,32 +8,6 @@ var $ = require('../bower_components/jquery/dist/jquery.min.js'),
     login = require('./user'),
     userid = ""
 
-// TEST MOVIE OBJECT
-// var data = {
-//   "Search": [
-//     {
-//       "Title": "Captain America: The First Avenger",
-//       "Year": "2011",
-//       "imdbID": "tt0458339",
-//       "Type": "movie",
-//       "Poster": "http://cdn.playbuzz.com/cdn/8ace370e-0453-4890-8fea-995f32ac9530/5efa0fd9-4850-47d4-814b-af367ab3f973.jpg"
-//     },
-//     {
-//       "Title": "Captain America: The Winter Soldier",
-//       "Year": "2014",
-//       "imdbID": "tt1843866",
-//       "Type": "movie",
-//       "Poster": "http://deeragaming.net/wp-content/uploads/2014/03/Bucky-Barnes-winter-soldier-image-bucky-barnes-winter-soldier-36483025-1200-1800.png"
-//     },
-//     {
-//       "Title": "Captain America: Civil War",
-//       "Year": "2016",
-//       "imdbID": "tt3498820",
-//       "Type": "movie",
-//       "Poster": "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg"
-//     }
-//   ]
-// }
 /* USE TO TOGGLE VIEWS BETWEEN PAGES WHILE TESTING
     UNCOMMENT ONE TO SEE THE OTHER
     EX: COMMENT OUT $('.LOGINPAGE') AND UNCOMMENT
@@ -60,38 +34,52 @@ $('#watchedmovies').hide()
 $('#unwatchedmovies').hide()
 $('#unwatched').hide()
 $('#watched').hide()
+$('#favorite').hide()
 
 $('#home').on('click', function(){
   $('#homemovies').show()
   $('#watchedmovies').hide()
   $('#unwatchedmovies').hide()
+  $('#fave').hide()
 
   $('#crumbs').html('Home')
   $('#home').addClass('active')
-  $('#watched, #unwatched').removeClass('active')
-  $('.instruction').text('Click on a movie to save it to your unwatched list!')
+  $('#watched, #unwatched, #favorite').removeClass('active')
 })
 
 $('#unwatched').on('click', function(){
   $('#unwatchedmovies').show()
   $('#watchedmovies').hide()
   $('#homemovies').hide()
+  $('#fave').hide()
 
   $('#crumbs').html('Unwatched')
   $('#unwatched').addClass('active')
-  $('#watched, #home').removeClass('active')
-  $('.instruction').text('Click a movie to save it to your watched list!')
+  $('#watched, #home, #favorite').removeClass('active')
 })
 
 $('#watched').on('click', function(){
   $('#watchedmovies').show()
   $('#homemovies').hide()
   $('#unwatchedmovies').hide()
+  $('#fave').hide()
 
   $('#crumbs').html('Watched')
   $('#watched').addClass('active')
-  $('#home, #unwatched').removeClass('active')
+  $('#home, #unwatched, #favorite').removeClass('active')
 })
+
+$('#favorite').on('click', function(){
+  $('#watchedmovies').hide()
+  $('#homemovies').hide()
+  $('#unwatchedmovies').hide()
+  $('#fave').show()
+
+  $('#crumbs').html('Favorites')
+  $('#favorite').addClass('active')
+  $('#home, #unwatched, #watched').removeClass('active')
+})
+
 //GOOGLE LOGIN
 $("#logout").on('click', function() {
   console.log("clicked auth");
@@ -104,99 +92,11 @@ $("#logout").on('click', function() {
     $('.afterLogin').show();
     $('#unwatched').show()
     $('#watched').show()
+    $('#favorite').show()
     // var token = result.credential.accessToken;
   })
 });
 
-/////////////////////////////////////////////////////
-// ADDS TEST MOVIES TO DOM
-// dom.addToDom(data)
-
-//PROMISE TO ADD SEARCH RESULTS TO DOM POSSIBLY GOES HERE?
-
-//////////////
-// USED TO PASS AN OBJECT INTO ADD SONG FIREBASE FUNCTION
-function buildObject(t, p, y){
-  let songObj = {
-    title: t,
-    poster: p,
-    year: y,
-    rating: "",
-    userid: userid
-  }
-  console.log(songObj)
-  return songObj
-}
-// CREATES DELETE BUTTON
-function options(){
-  var destroy = `<span class='delete glyphicon glyphicon-remove'></span>`
-  return destroy
-}
-
-// ADDS MOVIE TO UNWATCHED LIST WHEN CLICKED
-  $('.homemovies').on('click', '.add', function(e){
-    // $(this).parent().remove()
-    // console.log('HERE', $(this).find('p').html())
-    var jtarget = $(e.currentTarget).get(0)
-    var tpar = $(jtarget).parent().get(0)
-    $(this).remove()
-
-    var rate = `<div class='ratings' id='ratings'><input class='rating' id='rating'
-      type='range' step='.5' value='0' min='0' max='10'><span class='r_value'>0</span></div>`
-
-    $(tpar).append(rate)
-
-    $(this).removeClass('movie')
-    $(this).addClass('newUnwatched')
-    $(this).prepend(options())
-
-  $('.ratings').on('input', function(){
-    $(this).children('.r_value').html($('#rating', this).val())
-  })
-
-    $('#unwatchedmovies').append(this)
-
-    $('.delete').on('click', function(){
-    // Materialize.toast('Movie added to unwatched list!', 4000)
-    $(this).parent().remove()
-    //DELETE ELEMENT FROM UNWATCHED FIREBASE LIST PROMISE
-    // GOES HERE///////////////////////
-  })
-  // CREATES OBJECT BASED ON THE MOVIE CLICKED
-    let title = $(this).children('.movie_title').text()
-    let poster = $(this).children('.poster').attr('src')
-    let year = $(this).children('.year').text()
-
-    console.log(title, poster, year)
-    //PROMISE TO ADD TO FIREBASE UNWATCHED MOVIES TABLE GOES HERE
-    api.addUnwatchedMovie(buildObject(title, poster, year))
-  })
-  ///////////////////////////////////////////////
-    // ADDS MOVIE TO WATCHED LIST WHEN CLICKED
-$('.unwatchedmovies').on('click', "div", function(){
-  $(this).addClass('newWatched')
-  $(this).removeClass('newUnwatched')
-
-  // Materialize.toast('Movie added to watched list!', 4000)
-    //APPENDS MOVIE FROM UNWATCHED LIST TO WATCH LIST ON CLICK
-  $('#watchedmovies').append(this)
-
-  //PROMISE TO ADD TO FIREBASE WATCHED MOVIES TABLE GOES HERE
-/////////////////
-  $('.delete').on('click', function(){
-    $(this).parent().remove()
-  //DELETE ELEMENT FROM WATCHED FIREBASE LIST PROMISE
-    // GOES HERE
-  })
-
-  let title = $(this).find('.movie_title').text()
-  let poster = $(this).find('.poster').attr('src')
-  let year = $(this).find('.year').text()
-
-  console.log(title, poster, year)
-  api.addWatchedMovie(buildObject(title, poster, year))
-})
-//MOVIE SEARCH PROMISE
 $('#movieSearch').keypress(function(e) {
   if(e.which == 13) {
     $('div#homemovies').html("")
@@ -209,29 +109,110 @@ $('#movieSearch').keypress(function(e) {
             data.Search[key].id = key
           }
         })
-        dom.addToDom(data)
-        // movieEvents()
-      })
+        dom.addSearchToDom(data)
+        }).then(function(){
+          api.loadAllMovies()
+        }).then(function(data){
+          dom.addYoursToDom(data)
+        })
   }
 });
+
+  api.loadAllMovies()
+    .then(function(data){
+      dom.addYoursToDom(data)
+  })
+
+//PROMISE TO ADD SEARCH RESULTS TO DOM POSSIBLY GOES HERE?
+
+//////////////
+// USED TO PASS AN OBJECT INTO ADD SONG FIREBASE FUNCTION
+function buildObject(t, p, y, w){
+  let songObj = {
+    title: t,
+    poster: p,
+    year: y,
+    rating: "",
+    watched: w,
+    userid: userid
+  }
+  console.log(songObj)
+  return songObj
+}
+// CREATES DELETE BUTTON
+function options(){
+  var destroy = `<span class='delete glyphicon glyphicon-remove'></span>`
+  return destroy
+}
+
+// ADDS MOVIE TO UNWATCHED LIST WHEN CLICKED
+$('.homemovies').on('click', '.add', function(e){
+  var jtarget = $(e.currentTarget).get(0)
+  var tpar = $(jtarget).parent().get(0)
+  $(this).remove()
+
+  var rate = `<div class='ratings' id='ratings'><input class='rating' id='rating'
+    type='range' step='.5' value='0' min='0' max='10'><span class='r_value'>0</span></div>`
+
+  $(tpar).append(rate)
+
+  $('.ratings').on('input', function(){
+    $(this).children('.r_value').html($('#rating', this).val())
+  })
+  // Materialize.toast('Movie added to unwatched list!', 4000)
+  // CREATES OBJECT BASED ON THE MOVIE CLICKED
+  // let title = $(this).closest('.card').html()
+  $('.homemovies').on('click', '.movie', function(){
+  $(this).addClass('newUnwatched')
+    var title = $('.movie_title', this).text()
+    let poster = $('.poster', this).attr('src')
+    let year = $('.year', this).text()
+      $(this).removeClass('movie')
+    // console.log("here", title)
+    // console.log("poster",poster)
+    // console.log("year", year)
+    //PROMISE TO ADD TO FIREBASE UNWATCHED MOVIES TABLE GOES HERE
+    api.addMovie(buildObject(title, poster, year, false))
+      .then(function(data){
+        api.loadAllMovies()
+      })
+      .then(function(movie){
+        dom.addYoursToDom(movie)
+      }).then(function(){
+        api.loadAllMovies()
+        })
+  })
+})
+
+/////////////////////////////////////////////////////////////////
+    // ADDS MOVIE TO WATCHED LIST WHEN CLICKED
+$('.unwatchedmovies').on('click', ".save", function(){
+
+  // Materialize.toast('Movie added to watched list!', 4000)
+  var bool = false
+  if($('.rating').val() > 0){
+    bool = true
+  }
+
+  let title = $(this).find('.title').text()
+  let poster = $(this).find('.poster').attr('src')
+  let year = $(this).find('.year').text()
+
+  console.log("here", title, poster, year)
+
+  api.addMovie(buildObject(title, poster, year, bool))
+    .then(function(data){
+      api.loadAllMovies()
+    }).then(function(movie){
+      dom.addYoursToDom(movie)
+    }).then(function(){
+      api.loadAllMovies()
+    })
+})
+//MOVIE SEARCH PROMISE
 
 // CONVERTS MOVIE USER INPUT STRING TO A URL USEABLE ONE
 function convertString(string){
     var replaced = string.split(' ').join('+').toLowerCase()
     return replaced
 }
-
-// movieEvents()
-
-
-
-/*
-  1.search firebase and omdb in search bar
-  2.add favorites tab: rating 1-10
-     a.only in favorites if watched and rated 10
-  3.add rating on unwatched list
-  4.
-  4. buttons disabled until movie search on signin
-  5. must rate one of tracked movies as a 10 to showup as a favorite, when favorites
-     button is clicked
-*/
