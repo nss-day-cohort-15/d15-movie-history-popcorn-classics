f75474ad163cc336542d6873362e48970c43b4d7
Jacob Keen <jaclkeen@gmail.com>
2016-09-04 15:24:23 -0500

    filters based on userid

--- js/dom-builder.js
+++ js/dom-builder.js
@@ -33,31 +33,34 @@ function addSearchToDom(data){
   }
 }
 
-function addYoursToDom(data, id){
+function addYoursToDom(data, id, uid){
   $('div#unwatchedmovies').html("")
   $('div#watchedmovies').html("")
- var i = 0
- for(var key in data){
-  // console.log('ID', id[i])
-  var your_movie = $(`<div class='movie' id='${id[i]}'></div`)
-  var destroy = `<span class='delete glyphicon glyphicon-remove'></span>`
-  var title = `<h5 class='movie_title'>${data[key].title}</h2>`
-  var poster = `<img src='${data[key].poster}' class='poster'>`
-  var year = `<p class='year'>${data[key].year}</p>`
-  var rate = `<div class='ratings' id='ratings'><input class='rating' id='rating'
-    type='range' step='.5' value='${data[key].rating}' min='0' max='10'><span class='r_value'>${data[key].rating}</span></div>`
-  var add = `<p><a href='#' class='save' id='save'>Save Rating</a></p>`
+  console.log('UIDDDDDDDDDD', uid)
+  var i = 0
+  for(var key in data){
+    if(data[key].userid === uid){
+      console.log('TRUE')
+      var your_movie = $(`<div class='movie' id='${id[i]}'></div`)
+      var destroy = `<span class='delete glyphicon glyphicon-remove'></span>`
+      var title = `<h5 class='movie_title'>${data[key].title}</h2>`
+      var poster = `<img src='${data[key].poster}' class='poster'>`
+      var year = `<p class='year'>${data[key].year}</p>`
+      var rate = `<div class='ratings' id='ratings'><input class='rating' id='rating'
+        type='range' step='.5' value='${data[key].rating}' min='0' max='10'><span class='r_value'>${data[key].rating}</span></div>`
+      var add = `<p><a href='#' class='save' id='save'>Save Rating</a></p>`
 
-  your_movie.append(destroy + title + poster + year + rate + add)
+      your_movie.append(destroy + title + poster + year + rate + add)
 
-  if(data[key].watched === false && data[key].rating < 1){
-    $('#unwatchedmovies').append(your_movie)
-    console.log('unwatched')
-  }
-  else if(data[key].watched === true || data[key].rating > 0){
-    $('#watchedmovies').append(your_movie)
-    console.log('watched')
-  }
+      if(data[key].watched === false && data[key].rating < 1){
+        $('#unwatchedmovies').append(your_movie)
+        console.log('unwatched')
+      }
+      else if(data[key].watched === true || data[key].rating > 0){
+        $('#watchedmovies').append(your_movie)
+        console.log('watched')
+      }
+    }
   i++
  }
 
